ce55a70bbb639705efaf54688a9bd8c54e297c60
kabi10shrestha <kabi10shrestha@yahoo.com>
2024-10-01 21:57:05 -0400

    second class exercises

--- src/pages/qcomps/recipes.tsx
+++ src/pages/qcomps/recipes.tsx
@@ -12,11 +12,33 @@ export const recipes = [{
   ingredients: new Set(['chickpeas', 'olive oil', 'garlic cloves', 'lemon', 'tahini'])
 }];
 
+function IngredientItemList({ ingredients }: { ingredients: Array<string> }) {
+  return (
+    <ul>
+      {ingredients.map((ingredient) => (
+        <li key={ingredient}>{ingredient}</li>
+      ))}
+    </ul>
+  );
+}
+function RecipeDiv({ name, ingredients }: { name: string; ingredients: Array<string> }) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <IngredientItemList ingredients={ingredients} />
+    </div>
+  );
+}
+
 export default function RecipeList() {
+  const recipeDivList = recipes.map((recipe) => (
+    <RecipeDiv key={recipe.id} name={recipe.name} ingredients={Array.from(recipe.ingredients)} />
+  ));
+
   return (
     <div>
       <h1>Recipes</h1>
-      {}
+      {recipeDivList}
     </div>
   );
 }
