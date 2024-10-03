eee469e8d5fd67990a5a8043a31793066b1c0389
kabi10shrestha <kabi10shrestha@yahoo.com>
2024-10-03 16:06:26 -0400

    react state exercise

--- src/pages/qcomps/arrObj.tsx
+++ src/pages/qcomps/arrObj.tsx
@@ -17,7 +17,7 @@ export default function BucketList() {
   function handleToggleMyList(artworkId: number, nextSeen: boolean) {
     const tmpList = myList.map(e => {
         if (e.id === artworkId) {
-            e.seen = nextSeen
+          return{...e, seen:nextSeen}
         }
         return e
     });
@@ -27,7 +27,7 @@ export default function BucketList() {
   function handleToggleYourList(artworkId: number, nextSeen: boolean) {
     const tmpList = yourList.map(e => {
         if (e.id === artworkId) {
-            e.seen = nextSeen
+          return{...e, seen:nextSeen}
         }
         return e
     });
